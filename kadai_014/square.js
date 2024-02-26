76230dce7946ebe2f502cc3efdb3a4ad67f8632d
n-studio-firma <159144250+n-studio-firma@users.noreply.github.com>
2024-02-27 00:57:08 +0900

    Update square.js

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,8 +1,8 @@
 // 与えられた引数numを2倍にし、おの値を戻り値として返す関数を定義する
 
-const double = (num)=> {
-  return num *2;
+const square = (num)=> {
+  return num **2;
 }
 
 // 関数の戻り値を出力する
-console.log(double(50));
+console.log(square(50));
